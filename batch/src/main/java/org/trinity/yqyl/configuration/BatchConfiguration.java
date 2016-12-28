ced4716d604b1ce783ec5c53d52540376f1b8778
Isaiah Liu <zqliu@hengtiansoft.com>
2016-12-28 11:15:23 +0800

    1

--- batch/src/main/java/org/trinity/yqyl/configuration/BatchConfiguration.java
+++ batch/src/main/java/org/trinity/yqyl/configuration/BatchConfiguration.java
@@ -1,14 +1,19 @@
 package org.trinity.yqyl.configuration;
 
 import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
 import org.trinity.batch.configuration.InMemoryBatchConfigurer;
 
 @Configuration
 public class BatchConfiguration {
+    @Autowired
+    private ResourceLoader resourceLoader;
+
     @Bean
     public BatchConfigurer getBatchConfiguration() {
-        return new InMemoryBatchConfigurer();
+        return new InMemoryBatchConfigurer(resourceLoader);
     }
 }
