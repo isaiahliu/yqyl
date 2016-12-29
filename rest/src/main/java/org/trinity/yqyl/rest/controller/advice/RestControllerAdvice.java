abab1f5e37a30518c9a680e8d5d96ecbbc2331c4
Isaiah Liu <zqliu@hengtiansoft.com>
2016-12-29 16:10:50 +0800

    1

--- rest/src/main/java/org/trinity/yqyl/rest/controller/advice/RestControllerAdvice.java
+++ rest/src/main/java/org/trinity/yqyl/rest/controller/advice/RestControllerAdvice.java
@@ -2,8 +2,8 @@ package org.trinity.yqyl.rest.controller.advice;
 
 import javax.validation.ConstraintViolationException;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,53 +21,53 @@ import org.trinity.rest.controller.AbstractRestController;
 
 @ControllerAdvice
 public class RestControllerAdvice extends AbstractRestController {
-	private static Logger logger = LogManager.getLogger(RestControllerAdvice.class);
+    private static Logger logger = LogManager.getLogger(RestControllerAdvice.class);
 
-	@Autowired
-	private IExceptionFactory exceptionFactory;
+    @Autowired
+    private IExceptionFactory exceptionFactory;
 
-	@Autowired
-	private IMessageResolverChain messageResolver;
+    @Autowired
+    private IMessageResolverChain messageResolver;
 
-	@ExceptionHandler(ConstraintViolationException.class)
-	@ResponseStatus(value = HttpStatus.OK)
-	@ResponseBody
-	public ResponseEntity<IResponse> processException(final ConstraintViolationException e) {
-		final DefaultResponse response = new DefaultResponse();
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(value = HttpStatus.OK)
+    @ResponseBody
+    public ResponseEntity<IResponse> processException(final ConstraintViolationException e) {
+        final DefaultResponse response = new DefaultResponse();
 
-		e.getConstraintViolations().stream().map(item -> item.getMessage()).forEach(item -> response.addError("", item));
+        e.getConstraintViolations().stream().map(item -> item.getMessage()).forEach(item -> response.addError("", item));
 
-		logger.error(e.getMessage(), e);
+        logger.error(e.getMessage(), e);
 
-		return createResponseEntity(response, HttpStatus.OK);
-	}
+        return createResponseEntity(response, HttpStatus.OK);
+    }
 
-	@ExceptionHandler(LocalizedException.class)
-	@ResponseStatus(value = HttpStatus.OK)
-	@ResponseBody
-	public ResponseEntity<IResponse> processException(final LocalizedException e) {
-		final DefaultResponse response = new DefaultResponse();
+    @ExceptionHandler(LocalizedException.class)
+    @ResponseStatus(value = HttpStatus.OK)
+    @ResponseBody
+    public ResponseEntity<IResponse> processException(final LocalizedException e) {
+        final DefaultResponse response = new DefaultResponse();
 
-		e.getErrorMessages().forEach(
-				item -> response.addError(item.getItem1().getMessageCode(), messageResolver.getMessage(item.getItem1(), item.getItem2())));
+        e.getErrorMessages().forEach(
+                item -> response.addError(item.getItem1().getMessageCode(), messageResolver.getMessage(item.getItem1(), item.getItem2())));
 
-		logger.error(e.getMessage(), e);
+        logger.error(e.getMessage(), e);
 
-		return createResponseEntity(response, HttpStatus.OK);
-	}
+        return createResponseEntity(response, HttpStatus.OK);
+    }
 
-	@ExceptionHandler(Throwable.class)
-	@ResponseStatus(value = HttpStatus.OK)
-	@ResponseBody
-	public ResponseEntity<IResponse> processException(final Throwable e) {
-		final DefaultResponse response = new DefaultResponse();
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(value = HttpStatus.OK)
+    @ResponseBody
+    public ResponseEntity<IResponse> processException(final Throwable e) {
+        final DefaultResponse response = new DefaultResponse();
 
-		response.addError(GeneralErrorMessage.UNKNOWN_EXCEPTION.getMessageCode(), exceptionFactory
-				.createException(GeneralErrorMessage.UNKNOWN_EXCEPTION, e.getClass().getName(), e.getMessage()).getMessage());
+        response.addError(GeneralErrorMessage.UNKNOWN_EXCEPTION.getMessageCode(), exceptionFactory
+                .createException(GeneralErrorMessage.UNKNOWN_EXCEPTION, e.getClass().getName(), e.getMessage()).getMessage());
 
-		logger.error(e.getMessage(), e);
+        logger.error(e.getMessage(), e);
 
-		return createResponseEntity(response, HttpStatus.OK);
-	}
+        return createResponseEntity(response, HttpStatus.OK);
+    }
 
 }
--- web/src/main/java/org/trinity/yqyl/web/controller/ajax/advice/AjaxControllerAdvice.java
+++ web/src/main/java/org/trinity/yqyl/web/controller/ajax/advice/AjaxControllerAdvice.java
@@ -1,7 +1,7 @@
 package org.trinity.yqyl.web.controller.ajax.advice;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
