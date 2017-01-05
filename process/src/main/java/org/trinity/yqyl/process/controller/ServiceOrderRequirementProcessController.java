3fa26097b402e30068924b71a6fdcf570c7c1b2a
isaia <isaia@DESKTOP-N4GCAFI>
2017-01-05 19:57:04 +0800

    1

--- process/src/main/java/org/trinity/yqyl/process/controller/ServiceOrderRequirementProcessController.java
+++ process/src/main/java/org/trinity/yqyl/process/controller/ServiceOrderRequirementProcessController.java
@@ -7,7 +7,6 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.trinity.common.exception.IException;
-import org.trinity.common.util.TrinityUtil;
 import org.trinity.yqyl.common.message.dto.domain.ServiceOrderRequirementDto;
 import org.trinity.yqyl.common.message.dto.domain.ServiceOrderRequirementSearchingDto;
 import org.trinity.yqyl.common.message.lookup.OrderOperation;
@@ -16,6 +15,7 @@ import org.trinity.yqyl.common.message.lookup.PaymentMethod;
 import org.trinity.yqyl.common.message.lookup.PaymentType;
 import org.trinity.yqyl.common.message.lookup.RecordStatus;
 import org.trinity.yqyl.common.message.lookup.ServiceOrderRequirementStatus;
+import org.trinity.yqyl.common.utils.YqylUtil;
 import org.trinity.yqyl.process.controller.base.AbstractAutowiredCrudProcessController;
 import org.trinity.yqyl.process.controller.base.IServiceOrderRequirementProcessController;
 import org.trinity.yqyl.repository.business.dataaccess.IServiceOrderOperationRepository;
@@ -30,62 +30,62 @@ import org.trinity.yqyl.repository.business.entity.User;
 
 @Service
 public class ServiceOrderRequirementProcessController extends
-        AbstractAutowiredCrudProcessController<ServiceOrderRequirement, ServiceOrderRequirementDto, ServiceOrderRequirementSearchingDto, IServiceOrderRequirementRepository>
-        implements IServiceOrderRequirementProcessController {
+		AbstractAutowiredCrudProcessController<ServiceOrderRequirement, ServiceOrderRequirementDto, ServiceOrderRequirementSearchingDto, IServiceOrderRequirementRepository>
+		implements IServiceOrderRequirementProcessController {
 
-    @Autowired
-    private IUserRepository userRepository;
+	@Autowired
+	private IUserRepository userRepository;
 
-    @Autowired
-    private IServiceOrderRepository serviceOrderRepository;
+	@Autowired
+	private IServiceOrderRepository serviceOrderRepository;
 
-    @Autowired
-    private IServiceOrderOperationRepository serviceOrderOperationRepository;
+	@Autowired
+	private IServiceOrderOperationRepository serviceOrderOperationRepository;
 
-    @Override
-    @Transactional(rollbackOn = IException.class)
-    public void catchRequirement(final Long entityId) throws IException {
-        final User user = userRepository.findOneByUsername(getCurrentUsername());
-        final ServiceInfo serviceInfo = user.getServiceSupplierClient().getServiceInfos().get(0);
+	@Override
+	@Transactional(rollbackOn = IException.class)
+	public void catchRequirement(final Long entityId) throws IException {
+		final User user = userRepository.findOneByUsername(getCurrentUsername());
+		final ServiceInfo serviceInfo = user.getServiceSupplierClient().getServiceInfos().get(0);
 
-        final ServiceOrderRequirement requirement = getDomainEntityRepository().findOne(entityId);
-        requirement.setStatus(ServiceOrderRequirementStatus.IN_PROGRESS);
-        getDomainEntityRepository().save(requirement);
+		final ServiceOrderRequirement requirement = getDomainEntityRepository().findOne(entityId);
+		requirement.setStatus(ServiceOrderRequirementStatus.IN_PROGRESS);
+		getDomainEntityRepository().save(requirement);
 
-        final ServiceOrder order = new ServiceOrder();
-        order.setAddress(requirement.getAddress());
-        order.setExpectedPaymentAmount(requirement.getPrice());
-        order.setPaymentMethod(PaymentMethod.POS);
-        order.setPaymentType(PaymentType.ONE_TIME);
-        order.setPhone(requirement.getPhone());
-        order.setPrice(requirement.getPrice());
-        order.setServiceOrderRequirement(requirement);
-        order.setServiceTime(requirement.getServiceTime());
-        order.setStatus(OrderStatus.REQUEST_GRABBED);
-        order.setUid(TrinityUtil.randomBase36Uuid());
-        order.setUser(requirement.getUser());
-        order.setServiceInfo(serviceInfo);
+		final ServiceOrder order = new ServiceOrder();
+		order.setAddress(requirement.getAddress());
+		order.setExpectedPaymentAmount(requirement.getPrice());
+		order.setPaymentMethod(PaymentMethod.POS);
+		order.setPaymentType(PaymentType.ONE_TIME);
+		order.setPhone(requirement.getPhone());
+		order.setPrice(requirement.getPrice());
+		order.setServiceOrderRequirement(requirement);
+		order.setServiceTime(requirement.getServiceTime());
+		order.setStatus(OrderStatus.REQUEST_GRABBED);
+		order.setUid(YqylUtil.randomServiceOrderId());
+		order.setUser(requirement.getUser());
+		order.setServiceInfo(serviceInfo);
 
-        serviceOrderRepository.save(order);
+		serviceOrderRepository.save(order);
 
-        final ServiceOrderOperation operation = new ServiceOrderOperation();
-        operation.setOperation(OrderOperation.REQUIREMENT_CAUGHT);
-        operation.setOperator(user.getUsername());
-        operation.setOrderStatus(OrderStatus.REQUEST_GRABBED);
-        operation.setServiceOrder(order);
-        operation.setStatus(RecordStatus.ACTIVE);
-        operation.setTimestamp(new Date());
+		final ServiceOrderOperation operation = new ServiceOrderOperation();
+		operation.setOperation(OrderOperation.REQUIREMENT_CAUGHT);
+		operation.setOperator(user.getUsername());
+		operation.setOrderStatus(OrderStatus.REQUEST_GRABBED);
+		operation.setServiceOrder(order);
+		operation.setStatus(RecordStatus.ACTIVE);
+		operation.setTimestamp(new Date());
 
-        serviceOrderOperationRepository.save(operation);
-    }
+		serviceOrderOperationRepository.save(operation);
+	}
 
-    @Override
-    protected void addRelationshipFields(final ServiceOrderRequirement entity, final ServiceOrderRequirementDto dto) throws IException {
-        entity.setUser(userRepository.findOneByUsername(getCurrentUsername()));
-    }
+	@Override
+	protected void addRelationshipFields(final ServiceOrderRequirement entity, final ServiceOrderRequirementDto dto) throws IException {
+		entity.setUser(userRepository.findOneByUsername(getCurrentUsername()));
+	}
 
-    @Override
-    protected boolean canAccessAllStatus() {
-        return true;
-    }
+	@Override
+	protected boolean canAccessAllStatus() {
+		return true;
+	}
 }
