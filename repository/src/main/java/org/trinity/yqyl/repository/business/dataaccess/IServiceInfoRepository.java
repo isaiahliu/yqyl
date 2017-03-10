cf35d578029d6268f0ceca282889b3f5691f731e
isaia <isaia@DESKTOP-N4GCAFI>
2017-03-10 23:08:31 +0800

    1

--- repository/src/main/java/org/trinity/yqyl/repository/business/dataaccess/IServiceInfoRepository.java
+++ repository/src/main/java/org/trinity/yqyl/repository/business/dataaccess/IServiceInfoRepository.java
@@ -23,70 +23,76 @@ import org.trinity.yqyl.repository.business.entity.ServiceSupplierClient_;
 import org.trinity.yqyl.repository.business.entity.User_;
 
 public interface IServiceInfoRepository extends IJpaRepository<ServiceInfo, ServiceInfoSearchingDto> {
-    @Override
-    default Page<ServiceInfo> query(final ServiceInfoSearchingDto searchingDto, final Pageable pagable) {
-        final Specification<ServiceInfo> specification = (root, query, cb) -> {
-            final List<Predicate> predicates = new ArrayList<>();
+	@Override
+	default Page<ServiceInfo> query(final ServiceInfoSearchingDto searchingDto, final Pageable pagable) {
+		final Specification<ServiceInfo> specification = (root, query, cb) -> {
+			final List<Predicate> predicates = new ArrayList<>();
 
-            if (searchingDto.getId() != null) {
-                predicates.add(cb.equal(root.get(ServiceInfo_.id), searchingDto.getId()));
-            }
+			if (searchingDto.getId() != null) {
+				predicates.add(cb.equal(root.get(ServiceInfo_.id), searchingDto.getId()));
+			}
 
-            if (!searchingDto.isSearchAll()) {
-                predicates.add(cb.equal(root.join(ServiceInfo_.serviceSupplierClient).join(ServiceSupplierClient_.user).get(User_.username),
-                        searchingDto.getCurrentUsername()));
-            }
+			if (!searchingDto.isSearchAll()) {
+				predicates.add(cb.equal(root.join(ServiceInfo_.serviceSupplierClient).join(ServiceSupplierClient_.user).get(User_.username),
+						searchingDto.getCurrentUsername()));
+			}
 
-            if (searchingDto.getServiceSupplierClientId() != null) {
-                predicates.add(cb.equal(root.join(ServiceInfo_.serviceSupplierClient).get(ServiceSupplierClient_.userId),
-                        searchingDto.getServiceSupplierClientId()));
-            }
+			if (searchingDto.getServiceSupplierClientId() != null) {
+				predicates.add(cb.equal(root.join(ServiceInfo_.serviceSupplierClient).get(ServiceSupplierClient_.userId),
+						searchingDto.getServiceSupplierClientId()));
+			}
 
-            if (!StringUtils.isEmpty(searchingDto.getName())) {
-                predicates.add(cb.like(root.get(ServiceInfo_.name), "%" + searchingDto.getName() + "%"));
-            }
+			if (!StringUtils.isEmpty(searchingDto.getName())) {
+				predicates.add(cb.or(cb.like(root.get(ServiceInfo_.name), "%" + searchingDto.getName() + "%"),
+						cb.like(root.join(ServiceInfo_.serviceSupplierClient).get(ServiceSupplierClient_.name),
+								"%" + searchingDto.getName() + "%")));
+			}
 
-            if (searchingDto.getCategoryId() != null && searchingDto.getCategoryId() > 0) {
-                predicates.add(cb.equal(root.join(ServiceInfo_.serviceCategory).get(ServiceCategory_.id), searchingDto.getCategoryId()));
-            }
+			if (searchingDto.getCategoryId() != null && searchingDto.getCategoryId() > 0) {
+				predicates.add(cb.equal(root.join(ServiceInfo_.serviceCategory).get(ServiceCategory_.id), searchingDto.getCategoryId()));
+			}
 
-            if (searchingDto.getParentCategoryId() != null && searchingDto.getParentCategoryId() > 0) {
-                predicates.add(cb.equal(root.join(ServiceInfo_.serviceCategory).join(ServiceCategory_.parent).get(ServiceCategory_.id),
-                        searchingDto.getParentCategoryId()));
-            }
+			if (searchingDto.getParentCategoryId() != null && searchingDto.getParentCategoryId() > 0) {
+				predicates.add(cb.equal(root.join(ServiceInfo_.serviceCategory).join(ServiceCategory_.parent).get(ServiceCategory_.id),
+						searchingDto.getParentCategoryId()));
+			}
 
-            if (!searchingDto.getStatus().isEmpty()) {
-                final In<ServiceStatus> in = cb.in(root.get(ServiceInfo_.status));
-                searchingDto.getStatus().forEach(item -> in.value(LookupParser.parse(ServiceStatus.class, item)));
-                predicates.add(in);
-            } else if (!searchingDto.isSearchAllStatus()) {
-                predicates.add(cb.equal(root.get(ServiceInfo_.status), ServiceStatus.ACTIVE));
-            }
+			if (!searchingDto.getStatus().isEmpty()) {
+				final In<ServiceStatus> in = cb.in(root.get(ServiceInfo_.status));
+				searchingDto.getStatus().forEach(item -> in.value(LookupParser.parse(ServiceStatus.class, item)));
+				predicates.add(in);
+			} else if (!searchingDto.isSearchAllStatus()) {
+				predicates.add(cb.equal(root.get(ServiceInfo_.status), ServiceStatus.ACTIVE));
+			}
 
-            switch (searchingDto.getCustomSortedBy()) {
-            case "sales":
-                query.distinct(true);
-                if ("desc".equals(searchingDto.getCustomSortedDirection())) {
-                    query.orderBy(cb.desc(root.join(ServiceInfo_.serviceInfoStastic, JoinType.LEFT).get(ServiceInfoStastic_.orderCount)));
-                } else {
-                    query.orderBy(cb.asc(root.join(ServiceInfo_.serviceInfoStastic, JoinType.LEFT).get(ServiceInfoStastic_.orderCount)));
-                }
-                break;
-            case "appraise":
-                query.distinct(true);
-                if ("desc".equals(searchingDto.getCustomSortedDirection())) {
-                    query.orderBy(cb.desc(root.join(ServiceInfo_.serviceInfoStastic, JoinType.LEFT).get(ServiceInfoStastic_.appraiseAvg)));
-                } else {
-                    query.orderBy(cb.asc(root.join(ServiceInfo_.serviceInfoStastic, JoinType.LEFT).get(ServiceInfoStastic_.appraiseAvg)));
-                }
-                break;
-            default:
-                break;
-            }
+			switch (searchingDto.getCustomSortedBy()) {
+				case "sales":
+					query.distinct(true);
+					if ("desc".equals(searchingDto.getCustomSortedDirection())) {
+						query.orderBy(
+								cb.desc(root.join(ServiceInfo_.serviceInfoStastic, JoinType.LEFT).get(ServiceInfoStastic_.orderCount)));
+					} else {
+						query.orderBy(
+								cb.asc(root.join(ServiceInfo_.serviceInfoStastic, JoinType.LEFT).get(ServiceInfoStastic_.orderCount)));
+					}
+					break;
+				case "appraise":
+					query.distinct(true);
+					if ("desc".equals(searchingDto.getCustomSortedDirection())) {
+						query.orderBy(
+								cb.desc(root.join(ServiceInfo_.serviceInfoStastic, JoinType.LEFT).get(ServiceInfoStastic_.appraiseAvg)));
+					} else {
+						query.orderBy(
+								cb.asc(root.join(ServiceInfo_.serviceInfoStastic, JoinType.LEFT).get(ServiceInfoStastic_.appraiseAvg)));
+					}
+					break;
+				default:
+					break;
+			}
 
-            return cb.and(predicates.toArray(new Predicate[0]));
-        };
+			return cb.and(predicates.toArray(new Predicate[0]));
+		};
 
-        return findAll(specification, pagable);
-    }
+		return findAll(specification, pagable);
+	}
 }
